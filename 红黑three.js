dd8004b82636640d8ea35579cf5c59707834f475
Joker <chenhua385148231@126.com>
2018-09-07 14:15:15 +0800

    f

--- 红黑three.js
+++ 红黑three.js
@@ -1,19 +1,5 @@
 // 二叉树多次插入新节点导致不平衡。二叉树查找的思想：查找所需的最大次数等同于二叉树的高度
 
-// 红黑树，
-
-// 1.节点是红色和黑色
-// 2.根节点是褐黑色
-// 3.每个叶子节点都是黑色的空节点(NIL节点)
-// 4.每个红色节点的两个子节点都是黑色(从每个叶子到跟的所有路经不能有两个连续的红色节点)
-// 5.从任意节点到其每个叶子的所有路经都包含相同数目的黑色节点
-
-
-// 红黑树从根节点到叶子的最长路经不会超过最短路经的2倍
-
-// 变色 旋转（左旋转和有旋转）
-
-// 
 
 const quickSort2 = function (nums) {
     if(nums.length < 2) return nums
@@ -35,7 +21,7 @@ const quickSort2 = function (nums) {
             .concat(quickSort2(rightArr))
 }
 
-const mergeSort2 = function(nums){
+const mergeSort = function(nums){
     let merge = function(left, right){
       var result = []
     
@@ -57,7 +43,8 @@ const mergeSort2 = function(nums){
     let right = nums.slice(middle)
   
     return merge(mergeSort(left), mergeSort(right))
-  }
+}
+
 
 const quickSort = function (nums) {
     var sort = function(low, high){
@@ -91,29 +78,6 @@ const quickSort = function (nums) {
     return nums
 }
   
-const mergeSort = function(nums){
-    let merge = function(left, right){
-      var result = []
-    
-      while(left.length > 0 && right.length > 0){
-        left[0] > right[0]
-          ? result.push(right.shift())
-          : result.push(left.shift())
-      }
-    
-      return result.concat(left).concat(right)
-    }
-  
-    if(nums.length < 2){
-      return nums
-    }
-  
-    let middle = Math.floor(nums.length / 2)
-    let left = nums.slice(0, middle)
-    let right = nums.slice(middle)
-  
-    return merge(mergeSort(left), mergeSort(right))
-}
 
 
 // var nums = [5, 6, 8, 2, -2, 100, 99, 98, 102.1]
@@ -150,3 +114,10 @@ function getLongestArr(nums){
 
 var nums = [100, 5, 6, 8, 2, -2, 99, 198, 200]
 getLongestArr(nums)
+
+
+var A = function (){
+    console.log('hello')
+}
+
+var a = new A()
