8128c3286a00b4b55327d09c23fab6c07a130876
Joker <chenhua385148231@126.com>
2018-09-27 18:03:31 +0800

    f
    Signed-off-by: Joker <chenhua385148231@126.com>

--- tree.js
+++ tree.js
@@ -2,7 +2,7 @@
 
 
 const quickSort2 = function (nums) {
-    if(nums.length < 2) return nums
+    if (nums.length < 2) return nums
 
     var midIndex = Math.floor(nums.length / 2)
     var middle = nums.splice(midIndex, 1)[0]
@@ -11,79 +11,42 @@ const quickSort2 = function (nums) {
 
     for (let i = 0; i < nums.length; i++) {
         const o = nums[i];
-        o < middle
-            ? leftArr.push(o)
-            : rightArr.push(o)
+        o < middle ?
+            leftArr.push(o) :
+            rightArr.push(o)
     }
 
     return quickSort2(leftArr)
-            .concat(middle)
-            .concat(quickSort2(rightArr))
+        .concat(middle)
+        .concat(quickSort2(rightArr))
 }
 
-const mergeSort = function(nums){
-    let merge = function(left, right){
-      var result = []
-    
-      while(left.length > 0 && right.length > 0){
-        left[0] > right[0]
-          ? result.push(right.shift())
-          : result.push(left.shift())
-      }
-    
-      return result.concat(left).concat(right)
+const mergeSort = function (nums) {
+    let merge = function (left, right) {
+        var result = []
+
+        while (left.length > 0 && right.length > 0) {
+            left[0] > right[0] ?
+                result.push(right.shift()) :
+                result.push(left.shift())
+        }
+
+        return result.concat(left).concat(right)
     }
-  
-    if(nums.length < 2){
-      return nums
+
+    if (nums.length < 2) {
+        return nums
     }
-  
+
     let middle = Math.floor(nums.length / 2)
     let left = nums.slice(0, middle)
     let right = nums.slice(middle)
-  
+
     return merge(mergeSort(left), mergeSort(right))
 }
 
 
-const quickSort = function (nums) {
-    var sort = function(low, high){
-        var i = low
-        var j= high
-        var base = nums[low]
 
-        if(i < j){
-            for (; i < j; j--) {
-                if(nums[j] < base){
-                    nums[i++] = nums[j]
-                    break;
-                }
-            }
-
-            for (; i < j; i++) {
-                if(nums[i] > base){
-                    nums[j] = nums[i]
-                    break;
-                }
-            }
-
-            nums[i] = base
-            sort(0, i - 1)
-            sort(i + 1, high)
-        }
-    }
-
-    sort(0, nums.length - 1)
-    console.log(nums);
-    return nums
-}
-
-
-
-
-
-// var nums = [5, 6, 8, 2, -2, 100, 99, 98, 102.1]
-// quickSort(nums)
 
 // var t1 = +new Date
 // for (let i = 0; i < 100000000; i++) {
@@ -93,20 +56,20 @@ const quickSort = function (nums) {
 // console.log(t2);
 
 
-function getLongestArr(nums){
+function getLongestArr(nums) {
     var finalArr = []
 
     for (let i = 0; i < nums.length; i++) {
         var tmpArr = [nums[i]]
 
         for (let j = i + 1; j < nums.length; j++) {
-            if(nums[j] > tmpArr[tmpArr.length - 1]){
+            if (nums[j] > tmpArr[tmpArr.length - 1]) {
                 tmpArr.push(nums[j])
 
-                if(tmpArr.length > finalArr.length){
+                if (tmpArr.length > finalArr.length) {
                     finalArr = tmpArr
                 }
-            }else{
+            } else {
                 break;
             }
         }
@@ -118,16 +81,16 @@ function getLongestArr(nums){
 var nums = [100, 5, 6, 8, 2, -2, 99, 198, 200]
 // getLongestArr(nums)
 
-function process(arr){
+function process(arr) {
     let count = 0
     for (let i = 0; i < arr.length - count; i++) {
-        if(arr[i] === 0){
+        if (arr[i] === 0) {
             count++
             for (let j = i; j < arr.length; j++) {
                 arr[j] = arr[j + 1]
             }
             arr[arr.length - 1] = 0
-            if(arr[i] === 0){
+            if (arr[i] === 0) {
                 i--
             }
         }
@@ -139,7 +102,7 @@ function process(arr){
 // process([1,0,2,3,0,0,8,9,0,0])
 
 
-function bigDataAdd(num1, num2){
+function bigDataAdd(num1, num2) {
     let maxLen = Math.max(num1.length, num2.length)
     num1 = num1.split('').reverse().join('')
     num2 = num2.split('').reverse().join('')
@@ -148,15 +111,15 @@ function bigDataAdd(num1, num2){
 
     for (let i = 0; i < maxLen; i++) {
         let tmpSum = (+num1[i] || 0) + (+num2[i] || 0) + flag
-        if(tmpSum > 9){
+        if (tmpSum > 9) {
             flag = 1
-        }else{
+        } else {
             flag = 0
         }
         sum.push(tmpSum % 10)
     }
 
-    if(flag == 1){
+    if (flag == 1) {
         sum.push(1)
     }
 
@@ -165,3 +128,80 @@ function bigDataAdd(num1, num2){
 
 
 bigDataAdd('999', '9999')
+
+
+function quickSort(array) {
+    function sort(low, high) {
+        var i = low;
+        var j = high;
+        var flag = array[low];
+
+        if (high > low) {
+            while (i < j) {
+                for (; i < j; j--) {
+                    if (array[j] < flag) {
+                        array[i++] = array[j]; //a[i] = a[j]; i += 1;
+                        break;
+                    };
+                }
+                for (; i < j; i++) {
+                    if (array[i] > flag) {
+                        array[j--] = array[i];
+                        break;
+                    }
+                }
+            }
+            array[i] = flag;
+            sort(low, i - 1);
+            sort(i + 1, high);
+        }
+    }
+    sort(0, array.length - 1);
+    return array;
+}
+
+var nums = [5, 6, 8, 2, -2, 100, 99, 98, 102.1]
+quickSort(nums)
+console.log(nums);
+
+
+var tt =
+{
+    "dataList": null,
+    "returnCode": 1,
+    "returnMsg": "请求成功！",
+    "data": {
+        "referenceNumber": "A55555666",
+        "checkIn": "2018/08/10",
+        "checkOut": "2018/08/11",
+        "roomDetailList": [{
+            "units": 1,
+            "roomType": "豪华房",
+            "roomTypeEng": "Deluxe",
+            "bedType": "大床/双床[两份早餐]",
+            "board": "两份早餐",
+            "boardEng": "Breakfast FOR 2 Pax",
+            "pax": 2,
+            "guestsDetailList": ["娃 哈哈 [中国]", "巧克 力 [中国]"]
+        }, {
+            "units": 2,
+            "roomType": "豪华房",
+            "roomTypeEng": "Deluxe",
+            "bedType": "大床/双床[两份早餐]",
+            "board": "两份早餐",
+            "boardEng": "Breakfast FOR 2 Pax",
+            "pax": 1,
+            "guestsDetailList": ["棒棒 糖 [中国]"]
+        }],
+        "hotelName": "北京丽都皇冠假日酒店  Crowne Plaza Beijing Lido",
+        "hotelAddress": "北京 朝阳区 将台路6号",
+        "hotelTel": "010-64373388",
+        "hotelFax": "010-64379858",
+        "cancellationPolicy": "2018年8月10日10:00后不可修改或取消，罚金为首晚房费.noshow罚全段",
+        "addBreakfast": "2018/08/10至2018/08/11儿童早1份",
+        "addBed": "2018/08/10至2018/08/11儿童床1份",
+        "addWBN": "2018/08/10至2018/08/11宽带1份",
+        "customerRequests": "取消条款:入住前0天10点不可修改或取消，罚金为首晚",
+        "emergencyContactsInner": "0755-33397777"
+    }
+}
